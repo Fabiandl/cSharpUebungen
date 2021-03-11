26433a85d0a761d19cbae03b10066f8d9ecf16ea
Fabian <fabian-fdl@web.de>
2021-03-11 12:28:41 +0100

    threads

---
+++ Y_ParallelSieb/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+
+// http://de.wikipedia.org/wiki/Sieb_des_Eratosthenes
+
+namespace Y_ParallelSieb {
+    class Program {
+        class SiebSimple {
+            public bool[] Sieb;
+
+            public long Size { get; set; }
+
+            public SiebSimple(long n) {
+                Size = n;
+                Sieb = new bool[Size];
+            }
+
+            public void Reset() {
+                for (long i = 0; i < Size; ++i)
+                    Sieb[i] = false;
+            }
+
+            // ganz einfache Variante!
+            public void CalcPrimes() {
+                Sieb[0] = Sieb[1] = true; // not prime
+                for (long i = 2; i < Size; ++i) {
+                    if (Sieb[i])
+                        continue;
+                    for (long k = i + i; k < Size; k += i)
+                        Sieb[k] = true;
+                }
+            }
+        }
+
+        static void Main(string[] args) {
+            const long n = 100;
+            SiebSimple S = new SiebSimple(n);
+            Stopwatch StopWatch = new Stopwatch();
+
+            S.Reset();
+            StopWatch.Start();
+            S.CalcPrimes();
+            StopWatch.Stop();
+            TimeSpan ts = StopWatch.Elapsed;
+
+            Console.WriteLine("Benötigte Zeit: {0:00}.{1:000} [s]", ts.Seconds, ts.Milliseconds);
+            Console.Write("Größten 10 Primzahlen:");
+            for (long i = n - 1, cnt = 0; i >= 0 && cnt < 10; --i)
+                if (!S.Sieb[i]) {
+                    Console.Write(" " + i);
+                    ++cnt;
+                }
+
+            Console.WriteLine();
+
+            // und jetzt parallel ... 
+            Stopwatch StopWatch2 = new Stopwatch();
+            StopWatch2.Start();
+            var boolParal = parallelSieb(n);
+            StopWatch2.Stop();
+            TimeSpan ts2 = StopWatch2.Elapsed;
+            
+            Console.WriteLine("Benötigte Zeit: {0:00}.{1:000} [s]", ts2.Seconds, ts2.Milliseconds);
+            Console.Write("Größten 10 Primzahlen:");
+            for (long i = n - 1, cnt = 0; i >= 0 && cnt < 10; --i)
+                if (!boolParal[i]) {
+                    Console.Write(" " + i);
+                    ++cnt;
+                }
+        }
+
+        private static bool[] parallelSieb(long n)
+        {
+            Sieb = new bool[n+1];
+            Sieb[0] = Sieb[1] = true; // not prime
+
+            WorkerSiebSimple w1 = new WorkerSiebSimple();
+            WorkerSiebSimple w2 = new WorkerSiebSimple();
+            WorkerSiebSimple w3 = new WorkerSiebSimple();
+            WorkerSiebSimple w4 = new WorkerSiebSimple();
+
+            Thread t1 = new Thread(() => w1.CalcPrimes(0, 25));
+            Thread t2 = new Thread(() => w2.CalcPrimes(26, 50));
+            Thread t3 = new Thread(() => w3.CalcPrimes(51, 75));
+            Thread t4 = new Thread(() => w4.CalcPrimes(76, 100));
+
+            t1.Start();
+            t2.Start();
+            t3.Start();
+            t4.Start();
+
+            t1.Join();
+            t2.Join();
+            t3.Join();
+            t4.Join();
+            
+            return Sieb;
+        }
+
+        static volatile bool[] Sieb;
+        
+        public static object first_lock = new object();
+        class WorkerSiebSimple {
+            
+            // ganz einfache Variante!
+            public void CalcPrimes(int a, int b) {
+                if (a == 0)
+                {
+                    a = 2;
+                }
+                for (long i = a; i <= b; ++i)
+                {
+                    lock (first_lock)
+                    {
+                        if (Sieb[i])
+                            continue;
+                        for (long k = i + i; k < b; k += i)
+                            Sieb[k] = true;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ x_Threads/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace x_Threads
+{
+    public class Program
+    {
+        static volatile int sum = 0;
+
+        public static void Main(string[] args)
+        {
+            DynamischeThreadsAnzahl();
+
+            festeThreadAnzahl();
+
+            Console.WriteLine($"Summe ist {sum}");
+        }
+
+        private static void festeThreadAnzahl()
+        {
+            Worker w1 = new Worker();
+            Worker w2 = new Worker();
+            Worker w3 = new Worker();
+            Worker w4 = new Worker();
+
+            Thread t1 = new Thread(() => w1.summieren(0, 2500)) {Name = "t1"};
+            Thread t2 = new Thread(() => w2.summieren(2500, 5000)) {Name = "t2"};
+            Thread t3 = new Thread(() => w3.summieren(5000, 7500)) {Name = "t3"};
+            Thread t4 = new Thread(() => w4.summieren(7500, 10001)) {Name = "t4"};
+            Stopwatch StopWatch = new Stopwatch();
+            StopWatch.Start();
+            t1.Start();
+            t2.Start();
+            t3.Start();
+            t4.Start();
+
+            t1.Join();
+            t2.Join();
+            t3.Join();
+            t4.Join();
+            StopWatch.Stop();
+            TimeSpan ts = StopWatch.Elapsed;
+            Console.WriteLine("Benötigte Zeit: {0:00}.{1:000} [s]", ts.Seconds, ts.Milliseconds);
+        }
+
+        private static void DynamischeThreadsAnzahl()
+        {
+            int anzahlThr = 4;
+            int parts = 10000 / 4;
+            Thread[] tr = new Thread[4];
+            for (int i = 0; i < anzahlThr; i++)
+            {
+                int a = i * parts;
+                int b = a + parts;
+                if (i == 3)
+                {
+                    b += 1;
+                }
+
+                Console.WriteLine($"Thread {i} arbeitet von {a} bis ausschließlich {b}");
+                Worker w = new Worker();
+                Thread t = new Thread(() => w.summieren(a, b)) {Name = $"t{i}"};
+                tr[i] = t;
+            }
+            Stopwatch StopWatch = new Stopwatch();
+            StopWatch.Start();
+            for (int i = 0; i < anzahlThr; i++)
+            {
+                tr[i].Start();
+            }
+
+            for (int i = 0; i < anzahlThr; i++)
+            {
+                tr[i].Join();
+            }
+            StopWatch.Stop();
+            TimeSpan ts = StopWatch.Elapsed;
+            Console.WriteLine("Benötigte Zeit: {0:00}.{1:000} [s]", ts.Seconds, ts.Milliseconds);
+        }
+
+
+        public class Worker
+        {
+            public static object sum_lock = new object();
+
+            public void summieren(int a, int b)
+            {
+                Console.WriteLine($"Ich hab angefangen etwas zu machen {Thread.CurrentThread.Name}");
+                for (int i = a; i < b; i++)
+                {
+                    lock (sum_lock)
+                    {
+                        sum += i;
+                    }
+                }
+            }
+        }
+    }
+}
