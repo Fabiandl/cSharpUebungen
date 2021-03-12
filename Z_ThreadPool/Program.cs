9e185b82fa8cd8601e42dd0a3aad587cc38fe2c7
Fabian <fabian-fdl@web.de>
2021-03-12 10:00:00 +0100

    Tasks und so

---
+++ Z_ThreadPool/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace X_ThreadPool
+{
+    internal class Program
+    {
+
+
+        private static volatile List<int> aufgaben;
+        private static volatile List<int> results;
+        
+        public static void Main(string[] args)
+        {
+            aufgaben = new List<int>();
+            aufgaben.Add(1);
+            aufgaben.Add(2);
+            aufgaben.Add(3);
+            aufgaben.Add(4);
+            aufgaben.Add(5);
+            
+            results = new List<int>();
+         
+            StartMasterWorker(2);
+
+        }
+
+        private static void StartMasterWorker(int anzahlThreads)
+        {
+          
+            Thread[] threads = new Thread[anzahlThreads];
+            for (int i = 0; i < anzahlThreads; i++)
+            {
+                AufgabenWorker w = new AufgabenWorker();
+
+                Thread t = new Thread(w.calcMulti) {Name = $"Thread_{i}"};
+                t.Start();
+
+                threads[i] = t;
+            }
+
+            foreach (var t in threads)
+            {
+                t.Join();
+            }
+            Console.WriteLine($"Alle ergebnisse sind: {String.Join(",", results)}");
+        }
+
+
+        public class AufgabenWorker
+        {
+            public static object result_lock = new object();
+            public void calcMulti()
+            {
+                while (aufgaben.Count != 0)
+                {
+                    int zahl = aufgaben[0];
+                    aufgaben.RemoveAt(0);
+                    int res = zahl * zahl;
+
+                    lock (result_lock)
+                    {
+                        results.Add(res);   
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ zz_Tasks/Program.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace zz_Tasks
+{
+    internal class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            // iterativeTasks();
+
+            dynamicTasks(4);
+        }
+
+        private static void dynamicTasks(int anzahl)
+        {
+            object sum_lock = new object();
+            int sum = 0;
+
+            int part = 10000 / 4;
+
+            Task[] tasks = new Task[anzahl];
+
+            for (int i = 0; i < anzahl; i++)
+            {
+                int diff = 0;
+                if (i == anzahl-1)
+                {
+                    diff++;
+                }
+                int a = i * part;
+                int b = a + part;
+                int id = i+1;
+                Task t = Task.Run(
+                    () =>
+                    {
+                        Console.WriteLine($"-- T{id}: performing task with {a} : {b}");
+                        int temp = 0;
+                        for (int j = a; j < b + diff; j++)
+                        {
+                            temp += j;
+                        }
+
+                        lock (sum_lock)
+                        {
+                            Console.WriteLine($"T{id} temp = {temp}");
+                            sum += temp;
+                        }
+                    });
+                tasks[i] = t;
+            }
+
+            foreach (var t in tasks)
+            {
+                t.Wait();
+            }
+            Console.WriteLine($"Summe ist: {sum}");
+        }
+
+        private static void iterativeTasks()
+        {
+            object sum_lock = new object();
+            int sum = 0;
+            Task T1 = Task.Run(
+                () =>
+                {
+                    Console.WriteLine("-- T1: performing task");
+                    int temp = 0;
+                    for (int i = 0; i < 2500; i++)
+                    {
+                        temp += i;
+                    }
+
+                    lock (sum_lock)
+                    {
+                        Console.WriteLine($"T1 temp = {temp}");
+                        sum += temp;
+                    }
+                });
+            Task T2 = Task.Run(
+                () =>
+                {
+                    Console.WriteLine("-- T2: performing task");
+                    int temp = 0;
+                    for (int i = 2500; i < 5000; i++)
+                    {
+                        temp += i;
+                    }
+
+                    lock (sum_lock)
+                    {
+                        Console.WriteLine($"T2 temp = {temp}");
+                        sum += temp;
+                    }
+                });
+            Task T3 = Task.Run(
+                () =>
+                {
+                    Console.WriteLine("-- T3: performing task");
+                    int temp = 0;
+                    for (int i = 5000; i < 7500; i++)
+                    {
+                        temp += i;
+                    }
+
+                    lock (sum_lock)
+                    {
+                        Console.WriteLine($"T3 temp = {temp}");
+                        sum += temp;
+                    }
+                });
+            Task T4 = Task.Run(
+                () =>
+                {
+                    Console.WriteLine("-- T4: performing task");
+                    int temp = 0;
+                    for (int i = 7500; i < 10001; i++)
+                    {
+                        temp += i;
+                    }
+
+                    lock (sum_lock)
+                    {
+                        Console.WriteLine($"T4 temp = {temp}");
+                        sum += temp;
+                    }
+                });
+
+            T1.Wait();
+            T2.Wait();
+            T3.Wait();
+            T4.Wait();
+
+            Console.WriteLine($"Summe ist: {sum}");
+        }
+    }
+}
